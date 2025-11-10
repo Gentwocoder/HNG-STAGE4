cf4740abe4d72e0c2de22ca5565644a599e59498
gentwocoder <adetoyese0511@gmail.com>
2025-11-10 15:39:35 +0100

    fix: Update user API path to include versioning

--- hngstage4/hngstage4/urls.py
+++ hngstage4/hngstage4/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    path('api/users/', include('core.urls')),
+    path('api/v1/users/', include('core.urls')),
     path('health/', health_check, name='health-check'),
 ]
